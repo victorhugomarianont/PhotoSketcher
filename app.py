23bb0ca6a5cb7d4e48da5595c293522399d0a1f8
victorhugomarianont <63173422+victorhugomarianont@users.noreply.github.com>
2022-05-17 16:59:07 -0300

    Funções adicionadas: "Sketch" e  "Colored Draw"
    As funções dialogam com o novo escopo do projeto e introduzem as versões funcionais de processamento para "Rascunho" e "Desenho Colorido" a partir de uma dada imagem

--- app.py
+++ app.py
@@ -1,55 +1,48 @@
-import tkinter as tk
 import funcs
-f_selector = 0
-
-root = tk.Tk()
-root.geometry("600x500")
-canvas = tk.Canvas(root, width = 600,  height = 500)
-canvas.grid(rowspan = 12, columnspan = 12)
-root.iconbitmap("logo.ico")
-    
-def display_raw_img_button():
-    button_raw_img = tk.Button(root, text = "Search image", command = lambda:funcs.open_file(), height = 2, width = 10, pady = 15 )
-    button_raw_img.grid(column =5, row = 0)
-    
-    return button_raw_img
-    
-def display_selectors():     
-    f_selector = tk.IntVar()
-
-    r1 = tk.Radiobutton(root, text = "Sketch", variable = f_selector, value = 0 ).grid(column = 0, row = 3)
-    r2 = tk.Radiobutton(root, text = "Colored draw", variable = f_selector, value = 1 ).grid(column = 0, row = 4)
-    r3 = tk.Radiobutton(root, text = "Old photo", variable = f_selector, value = 2 ).grid(column = 0, row = 5)
-    
-    return f_selector    
-
-
-
-def display_new_image_button():
-    button_new_image = tk.Button(root, text = "Generate new image", command = lambda:retrieve_sel_op(), height = 2, width =10, pady = 15  )
-
-    button_new_image.grid (column = 0, row = 8)
-    
-    return button_new_image
-    
-def retrieve_sel_op():
-    sel = display_selectors()
-   
-    if sel.get() == 0:
-        func = funcs.sketch(funcs.img_path())
+import os
+r = True
+command = ""
+def inform_file():
+    f = input("informe o arquivo que passará \
+    \n por alteração: (certifique-se de que esteja na raiz do programa)")
+    if (os.path.isfile(f)):
+        return f
+    else:
         
-    elif sel.get() == 1:
-        pass
-    elif sel.get() == 2:
-       pass
- 
-    return func
+        return 0
 
+def menu():
     
-display_raw_img_button()
-display_selectors()
-#retrieve_sel_op()
-display_new_image_button()    
-#funcs.sketch(funcs.img_path())
-root.mainloop()
+    print("1. rascunho")
+    print("2.Desenho")
+    print("3.Efeito de foto antiga")
+    print("4. Ajuda")
+    print("5.Sair\n")
+    
+    print("Selecione alguma das opções acima para \
+    \n modificar imagens ou sair: \n")
+    return 1
 
+def run_app():
+    while r == True:
+        menu()
+        command = input("").lower()
+        # Note que apenas a função sketch está devidamente hailitada
+        if command == "1" or command == "rascunho":
+            f = inform_file()
+            print(f)
+            if f != 0:
+                funcs.sketch(f)
+        elif command == "2" or command == "desenho":
+            f = inform_file()
+            print (f)
+            if f != 0:
+                funcs.colored_draw(f)
+        elif command == "3" or command == "foto antiga":
+            pass
+        elif command == "4" or command == "ajuda":
+            pass
+            
+        
+    
+print(run_app())
--- funcs.py
+++ funcs.py
@@ -1,41 +1,11 @@
-#%matplotlib inline activate magic functions
+
 
 import cv2
 import matplotlib.pyplot as plt
-from tkinter.filedialog import askopenfile
-from tkinter import PhotoImage, Label
-from PIL import Image, ImageTk
-import os
-import sys
-image_path = ""
 
-
-def open_file():
-    
-   file_img = askopenfile(mode = "rb", filetypes = [("JPG Files","*.jpg"), ("PNG Files","*.PNG")])
-   if file_img:
-        loaded_img = ImageTk.PhotoImage(Image.open(file_img))
-        img = loaded_img
-        img_label = Label(image = img, bg = "white")
-        img_label.image = img
-        img_label.grid(column = 1, row =1)
-     
-        return img
-    
-def img_path():
-    path = sys.path.append(
-        os.path.abspath(
-            os.path.join(
-                os.path.dirname(__file__),
-                os.path.pardir
-            )
-        )
-    )
-    return path
-    
-
-def sketch(image_path):
-    img = cv2.imread(image_path,cv2.IMREAD_COLOR)
+# Função que transforma um arquivo válido de imagem dando-lhe um aspecto de desenho rascunhado
+def sketch(file_nm):
+    img = cv2.imread(file_nm,cv2.IMREAD_COLOR)
 
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -43,8 +13,9 @@ def sketch(image_path):
 
     img_gray_rev = cv2.GaussianBlur(img_gray_rev,(21,21),5,5)
     out = cv2.divide(img_gray, 255-img_gray_rev, scale = 230)
-
-    saved = cv2.imwrite("experimental.jpg", out)
+    # Para isso fazê-mos com que o software leia os tons de
+    # preto e branco e os inverta, delineando a imagem
+    saved = cv2.imwrite(f"sketch_{file_nm}", out)
     plt.imshow(out, cmap = "gray")
 
     plt.show()
@@ -52,8 +23,23 @@ def sketch(image_path):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     return saved
+
+def colored_draw(file_nm):
+    img = cv2.imread(file_nm, cv2.IMREAD_COLOR)
     
+    img_plot = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    
+    out = cv2.stylization(img, sigma_s=40, sigma_r=0.5)
+    out_plot = cv2.stylization(img_plot, sigma_s=40, sigma_r=0.5)
+    saved = cv2.imwrite(f"colored_draw_{file_nm}", out)
+    plt.imshow(out_plot)
+
+    plt.show()
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    return out
 
 if __name__ == "__main__":
-   image_path = img_path()
-   print(image_path)
+    print(colored_draw("HT.jpg"))
+   
